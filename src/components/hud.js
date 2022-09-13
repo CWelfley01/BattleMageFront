753833c8311b0bd41169dc75d8eaa18d81871e21
cwelfley01 <cwelfley01@gmail.com>
2022-09-13 18:52:54 -0400

    bug fix

--- src/components/hud.js
+++ src/components/hud.js
@@ -136,6 +136,8 @@ export default class Hud extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.state.element1 !== "blank") {
       if (prevState.element2 !== this.state.element2) {
+        if(this.state.element1 === this.state.element2)
+        return "BANG!!! Your spell backfired and killed you!!"}else{
         this.combineElements();
       }
       if (prevState.form !== this.state.form) {
